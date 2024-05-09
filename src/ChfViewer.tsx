794a47beb20e68ebcedf8e9fc09e311877358164
Diogo Trindade <diogotr7@gmail.com>
2024-05-09 17:41:44 +0100

    add more things

--- src/ChfViewer.tsx
+++ src/ChfViewer.tsx
@@ -1,40 +1,29 @@
-import { Text } from "@mantine/core";
-import { useEffect, useMemo, useState } from "react";
+import { Code, Skeleton, Text } from "@mantine/core";
+import { useEffect, useState } from "react";
 import { extract } from "./chf";
-import { parse } from "./character/StarCitizenCharacter";
+import { StarCitizenCharacter, parse } from "./character/StarCitizenCharacter";
 
 type Props = {
   chf: File;
 };
 
 function ChfViewer({ chf }: Props) {
-  const [bytes, setBytes] = useState<Uint8Array>();
+  const [character, setCharacter] = useState<StarCitizenCharacter | null>(null);
 
   useEffect(() => {
     chf.arrayBuffer().then((buffer) => {
-      setBytes(new Uint8Array(buffer));
+      const char = parse(extract(new Uint8Array(buffer)));
+      setCharacter(char);
     });
   }, [chf]);
 
-  const bytesAsHex = useMemo(() => {
-    if (!bytes) return;
-    return Array.from(bytes)
-      .map((byte) => byte.toString(16).padStart(2, "0"))
-      .join("");
-  }, [bytes]);
-
-  useEffect(() => {
-    if (!bytes) return;
-
-    const decompressed = extract(bytes);
-    const char = parse(decompressed);
-
-    console.log(char);
-  }, [bytes]);
-
   return (
     <>
-      <Text>Bytes: {bytesAsHex}</Text>
+      {character ? (
+        <Code block>{JSON.stringify(character, null, 2)}</Code>
+      ) : (
+        <Skeleton height={300} />
+      )}
     </>
   );
 }
