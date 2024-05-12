70fa6ed90e81d0b410c9c3f08af5d7055ddc8f79
Diogo Trindade <diogotr7@gmail.com>
2024-05-12 23:22:14 +0100

    fix tests

--- src/tests/read-chf.test.ts
+++ src/tests/read-chf.test.ts
@@ -3,7 +3,8 @@ import { readFile } from 'node:fs/promises'
 import path from 'node:path'
 import { readdirSync } from 'node:fs'
 import { assert, describe, it } from 'vitest'
-import { createChf, readChf } from '../Chf/ChfFile'
+import { createChf, extractChf } from '../Chf/ChfFile'
+import { readCharacter } from '../Chf/Character'
 
 const outputDir = path.join(path.dirname(__filename), 'test-chf')
 const testFiles = readdirSync(outputDir)
@@ -12,7 +13,7 @@ describe('read All Chf Files', () => {
   it.each(testFiles)('read %s', async (file) => {
     const buffer = await readFile(path.join(outputDir, file))
     assert.doesNotThrow(() => {
-      readChf(new Uint8Array(buffer))
+      readCharacter(extractChf(new Uint8Array(buffer)))
     })
   })
 })
@@ -21,9 +22,9 @@ describe('read, write, reread all Chf Files', () => {
   it.each(testFiles)('read %s', async (file) => {
     const buffer = await readFile(path.join(outputDir, file))
     assert.doesNotThrow(() => {
-      const char = readChf(new Uint8Array(buffer))
+      const char = readCharacter(extractChf(new Uint8Array(buffer)))
       const chfAgain = createChf(char)
-      readChf(new Uint8Array(chfAgain))
+      readCharacter(extractChf(new Uint8Array(chfAgain)))
     })
   })
 })
